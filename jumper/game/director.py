85fc9c5c59ee9ba1e853ff94746a8401f325be55
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-05 14:02:24 -0700

    added some self perameters

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -6,6 +6,15 @@ class Director():
     """
 
     def __init__(self):
+        """
+        
+        """
+        self.console = Console()
+        self.jumper = Jumper()
+        self.bard = Board()
+        self.board.debug_mode = False
+        self.keep_playing = True
+        self.current_guess = ''
         pass
 
     pass
