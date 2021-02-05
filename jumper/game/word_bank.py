c680b66c30e43d049e74099190825e6cdf0d420c
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-05 14:54:46 -0700

    added comments and stub functions

--- jumper/game/word_bank.py
+++ jumper/game/word_bank.py
@@ -1,9 +1,10 @@
 import random
 
 
-class WordBank():
+class WordBank:
     """
-    The class word...
+    The class WordBank deals with creating a word and randomly picking that word
+    to return to the class Screen.
     """
 
     def __init__(self):
@@ -14,13 +15,15 @@ class WordBank():
 
     def generate_word(self):
         """
-        The generate_word method has a list of words and pulls a random word from the
-        list to print to the screen.
+        The generate_word method contains a list of words to choose from.
         """
         pass
 
     def pick_random_word(self):
+        """
+        The pick_random_word method takes the list from generate_word and picks
+        a random one to return to Screen.
+        """
         pass
-
 
     pass
