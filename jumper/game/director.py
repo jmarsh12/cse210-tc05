14723e6fdf851086efd543281c4d4ab671e0a4cd
Craig Wright <craigers.wright@gmail.com>
2021-02-05 15:07:03 -0700

    added some method in director

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,3 +1,6 @@
+from game.rule_manager import RuleManager
+from game.screen import Screen
+from game.word_bank import WordBank
 
 
 class Director():
@@ -9,12 +12,34 @@ class Director():
         """
         
         """
-        self.console = Console()
-        self.jumper = Jumper()
-        self.bard = Board()
-        self.board.debug_mode = False
+        #self.console = Console()
+        #self.jumper = Jumper()
+       # self.bard = Board()
+        #self.board.debug_mode = False
         self.keep_playing = True
-        self.current_guess = ''
+        #self.current_guess = ''
         pass
 
-    pass
+    word1 = WordBank()
+    screen1 = Screen()
+    manager = RuleManager()
+
+    def start_game(self):
+        """
+        begins the game and continues running until the user decides to quit playing
+        """
+        while self.keep_playing and !manager.game_over: #TODO: make sure this is a method in rulemngr class
+            pass
+        pass
+
+    def update_values(self):
+        """
+        updates all values before outputting to screen
+        """
+        pass
+
+    def output(self):
+        """
+        prints to screen
+        """
+        pass
