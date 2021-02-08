201296a40e90d4b2eca0c2299def9ddc7c634892
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-02-08 13:57:38 -0700

    added words

--- jumper/game/word_bank.py
+++ jumper/game/word_bank.py
@@ -19,8 +19,14 @@ class WordBank:
         picks a random word to return to the user.
         """
         self.get_word = ['Intelligent', 'Supercalifragilisticexpialidocious', 'Gina', 'Pterodactyl', 'Giraffe',
-                         'Dragon',
-                         'Hippopotomonstrosesquippedaliophobia']
+                         'Dragon', 'Hippopotomonstrosesquippedaliophobia', 'Horizon', 'Dawn', 'Dinosaur', 'Arduino',
+                         'Tyrannosaurus', 'Refrigerator', 'Lion', 'Hydrogenous', 'Hemophobia', 'Claustrophobia',
+                         'Xenophobia', 'Circumlocution', 'Sesquipedalian',
+                         'Pneumonoultramicroscopicsilicovolcanoconiosis', 'Pseudopseudohypoparathyroidism',
+                         'Floccinaucinihilipilification', 'Dog', 'Cat', 'Bat', 'Rat', 'Wolf', 'Eagle',
+                         'Antidisestablishmentarianism', 'Rabbit', 'Paratrooper', 'Military', 'AR15', 'Construction',
+                         'Mantis', 'Jedi', 'Yoda', 'California', 'Idaho', 'Starfighter', 'Deathstar', 'Toyota',
+                         'Volkswagen', 'Jeep', 'Beep', 'Chevrolet', 'Corvette', 'Freighter', 'Porsche']
         word = random.choice(self.get_word)
 
         return word.upper()
