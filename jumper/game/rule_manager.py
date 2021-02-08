49d06c327769cb5e567fca1e6003e24032f3d496
Bryson <bryson.berrett19@gmail.com>
2021-02-08 13:32:56 -0700

    Rules Manager created

--- jumper/game/rule_manager.py
+++ jumper/game/rule_manager.py
@@ -1,11 +1,39 @@
+from screen import Screen
+import sys
 
-
-class RuleManager:
+class Rule_Manager():
     """
-    The class game...
+    The class Rule_Manager takes care of 
+    methods relating to the gameplay of the user.
     """
 
     def __init__(self):
-        pass
+        self.play = 0
 
-    pass
+    def check_letter(screen.letter, rand_word):
+        for i in rand_word
+            if screen.letter is == i:
+                return True
+            else:
+                return False
+        """
+        This function will take the letter that it recieves from screen and check
+        to see if it is a letter in the word that the player is guessing. 
+        It will return a true or false.
+        """
+
+    def wrong_answer():
+    """
+    This function keeps track of how many times 
+    the player has guessed the wrong answer.
+    """
+        if check_letter == False:
+             self.play += 1
+        
+    def keep_playing()
+    """
+    This function will run continously and determine 
+    whether or not you can keep playing.
+    """
+    while self.play >= 3
+    exit()
