2ee7647cd5ad06e21d3aaa50825ba6684e12621f
stonks4elon <76183757+stonks4elon@users.noreply.github.com>
2021-02-08 20:52:00 -0700

    added play again method

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -2,6 +2,7 @@ from game.rule_manager import Rule_Manager
 from game.screen import Screen
 from game.word_bank import WordBank
 
+
 class Director:
     """
     The class director...
@@ -15,24 +16,28 @@ class Director:
         self.screen = Screen()
         self.manager = Rule_Manager()
         self.keep_playing = True
+        self.play_another_time = 'y'
         self.letter = ""
         self.word = ""
         self.underscore = []
         self.jumper = []
-        
+
     def start_game(self):
         """
         begins the game and continues running until the user decides to quit playing
         returns: --- (only calls other functions)
         """
-        self.word = self.word_bank.get_word()
-        self.underscore = self.screen.get_underscore(len(self.word))
-        self.screen.display(len(self.word))
-        # TODO: make sure this is a method in rulemanager class:
-        while self.keep_playing and self.manager.game_over(self.word, self.underscore):
-            self.get_inputs()
-            self.update_values()
-            self.output()
+        while self.play_another_time == 'y' and self.keep_playing:
+            self.word = self.word_bank.get_word()
+            self.underscore = self.screen.get_underscore(len(self.word))
+            self.screen.display(len(self.word))
+
+            while self.keep_playing and self.manager.game_over(self.word, self.underscore):
+                self.get_inputs()
+                self.update_values()
+                self.output()
+
+            self.play_again()
 
     def get_inputs(self):
         self.letter = self.screen.get_letter()
@@ -55,3 +60,16 @@ class Director:
         returns: ---(only calls other functions)
         """
         self.screen.display(len(self.word))
+
+    def play_again(self):
+        choice = input("Would you like to play again? [y/n]:")
+        if choice == 'y':
+            self.letter = ""
+            self.word = ""
+            self.underscore = []
+            self.jumper = []
+            self.screen.__init__()
+            self.manager.__init__()
+            self.word_bank.__init__()
+        elif choice == 'n':
+            self.keep_playing = False
